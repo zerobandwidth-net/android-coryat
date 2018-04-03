60d48d8daa539fa43f74503c410ab5e9de71a253
zerobandwidth <pasha.paterson@gmail.com>
2018-04-03 16:02:06 -0400

    (#1) Setting up the project.

---
+++ appCoryat/src/main/java/net/zerobandwidth/android/apps/coryat/CoryatMainActivity.java
@@ -0,0 +1,13 @@
+package net.zerobandwidth.android.apps.coryat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoryatMainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.act_coryat_main);
+	}
+}
